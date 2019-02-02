b333fb7e565ca3b00571e433bdc6eea7c7499028
Shreenidhi7 <shreenidhisharma7@gmail.com>
2019-02-02 19:32:00 +0530

    11th commit

---
+++ DataStructures/calender.js
@@ -0,0 +1,51 @@
+var access = require('../Utility/calender')
+var M = require('../Utility/Utility');
+var T = require('util')
+
+function call() 
+{
+    var month = +process.argv[2];
+    var year = +process.argv[3];
+    
+    var week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    var dates = [0, 31, 28, 31, 30, 31, 31, 30, 31, 30, 31, 30, 31];
+   
+    var day = access.day(month,1,year);
+    console.log(day);
+    var leap = access.leapYear(year);
+    if (leap = true) 
+      {
+        dates[2] = 29;
+      }
+    for (var i = 0; i < week.length; i++) 
+      {
+        T.print(week[i] + "  ");
+      }
+    console.log();
+    
+    for (var i = 0; i < (day * 5); i++) 
+      {
+        T.print(" ");
+      }
+
+    for (var i = 1; i <= dates[month]; i++) 
+      {
+        if (i < 10) 
+          {
+            T.print(" " + i + "   ");
+          }
+
+        if (i > 9) 
+          {
+            T.print("" + i + "   ")
+          }
+        if ((i + day) % 7 == 0) 
+          {
+            console.log();
+          }
+        }
+
+    console.log("\n\n");
+
+} 
+call();
--- Utility/UtilDataStructures.js
+++ Utility/UtilDataStructures.js
@@ -621,13 +621,60 @@ print()
 }
 
 
+var utility = require('../Utility/utility')
+module.exports={
+   day(m,d,y)
+   {
+    var v = Math.floor((14 - m) / 12)
+    var y0 = y - v
+    var x = y0 + Math.floor(y0 / 4) - Math.floor(y0 / 100) + Math.floor(y0 / 400)
+    var m0 = m + 12 * Math.floor((14 - m) / 12) - 2
+    var d0 = (d + x + Math.floor((31 * m0) / 12)) % 7
+    d0 = Math.floor(d0)
+    return d0;
+   },
+   leapYear(year)
+   {
+    if  ((year % 4 == 0) && (year % 100 != 0)) return true;
+        if  (year % 400 == 0) return true;
+        return false;
+   },
+
+    monthof(month) {
+
+        switch (month) {
+            case 1: return 31;
+                break;
+            case 2: return 28;
+                break;
+            case 3: return 31;
+                break;
+            case 4: return 30;
+                break;
+            case 5: return 31;
+                break;
+            case 6: return 30;
+                break;
+            case 7: return 31;
+                break;
+            case 8: return 31;
+                break;
+            case 9: return 30;
+                break;
+            case 10: return 31;
+                break;
+            case 11: return 30;
+                break;
+            case 12: return 31;
+break;
+        }
+}
 
 
 
 
 
-
-
+}
 
 
 
@@ -646,7 +693,11 @@ print()
 
 
   
-module.exports = { LinkedList, Node, Stack,Queue,Dequeue,
+module.exports = { LinkedList, 
+  Node, 
+  Stack, 
+  Queue, 
+  Dequeue,
 
 getBinaryTree(num)
 {
